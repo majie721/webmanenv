30eb7da85d6071f357aae431361330d28e381ec1
majie721 <1440413448@qq.com>
2024-11-08 10:12:00 +0800

    Update helpers.php
    函数冲突+_env

--- src/helpers.php
+++ src/helpers.php
@@ -5,3 +5,10 @@ if(!function_exists('env')){
         return $loader::get($key,$default);
     }
 }
+
+if(!function_exists('_env')){
+    function _env(string $key = null, mixed $default = null){
+        $loader =  new WebmanEnv\EnvLoader(base_path('.env'));
+        return $loader::get($key,$default);
+    }
+}
